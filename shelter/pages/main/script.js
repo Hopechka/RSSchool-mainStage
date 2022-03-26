f3f5ec082074f59f9fbff7d908586820ae527fbc
Nadezhda Budarina <hopechka@gmail.com>
2022-03-26 20:16:38 +0300

    feat:add disableding btn for main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -3,17 +3,22 @@ const nextBtn = document.querySelector('.next');
 const slider = document.querySelector('.slider');
 
 let left = 0;
+
 function slideRight() {
   left = left - 360;
-  if (left < -1800) {
-    left = 0;
+  prevBtn.disabled = false;
+  if (left < -1440) {
+    nextBtn.disabled = true;
   }
   slider.style.left = left + 'px';
 }
+
 function slideLeft() {
   left = left + 360;
+  nextBtn.disabled = false;
   if (left > 0) {
-    left = -1800;
+    left = 0;
+    prevBtn.disabled = true;
   }
   slider.style.left = left + 'px';
 }
