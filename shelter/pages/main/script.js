e8773cb696f3096133c6647445b30ca3c35039f7
Nadezhda Budarina <hopechka@gmail.com>
2022-04-08 21:52:54 +0300

    feat:add pop-up menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -31,13 +31,28 @@ nextBtn.addEventListener('click', slideRight);
 const burgerBtn = document.querySelector('.burger-menu');
 const logo = document.querySelector('.logo');
 const nav = document.querySelector('.nav');
+const navList = document.querySelector('.nav-list');
+const slide = document.querySelector('.slider');
 const header = document.querySelector('#header');
+const navItem = document.querySelectorAll('.nav-item');
 
 function activateMenu() {
   burgerBtn.classList.toggle('active');
   logo.classList.toggle('active');
   nav.classList.toggle('active');
+  navList.classList.toggle('active');
+  slider.classList.toggle('active');
   header.classList.toggle('active');
 }
 
+function closeMenu() {
+  burgerBtn.classList.remove('active');
+  navList.classList.remove('active');
+  logo.classList.remove('active');
+  nav.classList.remove('active');
+  slider.classList.remove('active');
+  header.classList.remove('active');
+}
+
 burgerBtn.addEventListener('click', activateMenu);
+nav.addEventListener('click', closeMenu);
