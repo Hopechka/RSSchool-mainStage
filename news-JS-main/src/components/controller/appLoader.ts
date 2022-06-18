1089372021bf961255d90a291cb02a6da7cf13cd
Nadezhda Budarina <hopechka@gmail.com>
2022-06-18 13:39:27 +0300

    refactor: rename js files

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '96527e9e66f5469ba4cf1914dac88972', // получите свой ключ https://newsapi.org/
         });
     }
 }
