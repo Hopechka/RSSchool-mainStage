7f1e2e8f53e260ce86a70e6fcab2804b0a451b6b
Nadezhda Budarina <hopechka@gmail.com>
2022-07-06 19:49:21 +0300

    refactor: setup the environment for the task 1. setup eslint 2. setup webpack 3. setup sass 4. setup tsconfig 5. add normalize.css

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import 'normalize.css';
+import './style.css';
+import store from './assets/store.json';
+import photo1 from './assets/images/1.jpg';
+import photo2 from './assets/images/2.jpg';
+
+console.log(store.firstName);
+console.log(store.address);
+console.log(store.phoneNumbers[0]);
+
+function trySome(): void {
+    const a = 'Hello world';
+    const arr: number[] = [1, 2, 3];
+    console.log(a);
+    console.log(arr);
+}
+
+trySome();
+
+// const img = document.getElementById('img') as HTMLElement;
+
+// const pic = new Image();
+// pic.src = photo as HTMLImageElement;
+// console.log(pic);
+
+// img.appendChild(pic);
+
+const div = document.querySelector('div') as HTMLDivElement;
+
+let i = 1;
+function picChange() {
+    if (i === 1) {
+        i++;
+        div.style.background = `url('${photo2 as '*.jpg'}')`;
+    } else {
+        i--;
+        div.style.background = `url('${photo1 as '*.jpg'}')`;
+    }
+    div.style.backgroundSize = 'cover';
+    div.style.backgroundRepeat = 'no-repeat';
+}
+
+const btn = document.querySelector('.btn') as Element;
+
+btn.addEventListener('click', picChange);
